cc53352657734a6a49a560d6cf10d45ab1319022
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2025-11-03 23:35:10 +0900

    add file post-sync.py

--- hooks/private.py
+++ hooks/private.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+import os
+import subprocess
+import getpass
+
+
 def main(repo_topdir=None, **kwargs):
     """Main function invoked directly by repo.
 
@@ -8,5 +13,23 @@ def main(repo_topdir=None, **kwargs):
       repo_topdir: The absolute path to the top-level directory of the repo workspace.
       kwargs: Leave this here for forward-compatibility.
     """
+
     print(f"Post-sync hook executed in repo at {repo_topdir}")
     print("This is the podman_setting post-sync hook.")
+
+    print(f"user name is {getpass.getuser()}")
+
+    PODMAN_HOOK_ROOT = repo_topdir + "/setting/podman"
+    HOOK_FILENAME = {"post-sync": "post-sync.py", "None": "None"}
+    os.chdir(PODMAN_HOOK_ROOT)
+
+    subprocess.run(
+        [
+            "python3",
+            PODMAN_HOOK_ROOT + "/file/" + HOOK_FILENAME["post-sync"],
+            f"{repo_topdir}/podman/file",
+        ]
+    )
+
+    # script4podman_file = runpy.run_path("file/" + HOOK_FILENAME["post-sync"])
+    # script4podman_file["main"](repo_topdir)
--- podman/file/post-sync.py
+++ podman/file/post-sync.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python
+from pathlib import Path
+import os, sys, getpass, shutil
+
+SYSTEMD_DIR = f"/home/{getpass.getuser()}/.config/containers/systemd"
+
+
+class TargetDir:
+    def __init__(self, path=""):
+        self.path = path
+
+    def getPath(self, launchedFromRepo=False) -> Path:
+        if launchedFromRepo:
+            print("This script isn't being run directly.")
+            ret = Path(os.path.dirname(Path(__file__).resolve()))
+        else:
+            print("This script is being run directly.")
+            ret = Path(self.path)
+        return ret
+
+
+def mainProcess(dir, launchedFromRepo):
+    """個人向け設定反映用プログラム
+
+    Args:
+        dir (_type_): ディレクトリパス
+        launchedFromRepo (bool): repo経由か否かを判別
+    """
+    print("This is the hook for file pod.")
+    td = TargetDir(dir)
+    p = td.getPath(launchedFromRepo)
+    pod_path = list(Path(p).rglob("Quadlet/*"))
+    for i in pod_path:
+        shutil.copyfile(i.resolve(), f"{SYSTEMD_DIR}/{i.name}")
+        print(f"copy {i.name} to {SYSTEMD_DIR}")
+
+
 def main(repo_topdir=None, **kwargs):
     """Main function invoked directly by repo.
 
@@ -9,4 +45,9 @@ def main(repo_topdir=None, **kwargs):
       kwargs: Leave this here for forward-compatibility.
     """
     print(f"Post-sync hook executed in repo at {repo_topdir}")
-    print("This is the hook for file pod.")
+    mainProcess(repo_topdir, True)
+
+
+if __name__ == "__main__":
+    print(f"main ok->{sys.argv[1]}")
+    mainProcess(sys.argv[1], False)
